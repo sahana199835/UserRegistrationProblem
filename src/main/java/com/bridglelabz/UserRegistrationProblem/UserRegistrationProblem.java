bcf2474879efe512152f1362d295ebabc5b0d273
sahana199835 <tnsahana1@gmail.com>
2023-04-15 17:59:58 +0530

    add:.gitignore and README.md files are added

---
+++ src/main/java/com/bridglelabz/UserRegistrationProblem/UserRegistrationProblem.java
@@ -0,0 +1,31 @@
+package com.bridglelabz.UserRegistrationProblem;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistrationProblem {
+
+	public static void main(String[] args) {
+		String regex = "^[A-Z][a-zA-Z]{2,}$";
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The First Name : ");
+		String data = sc.nextLine();
+
+		Pattern pattern = Pattern.compile(regex);
+
+		Matcher matcher = pattern.matcher(data);
+		boolean result = matcher.matches();
+
+		if (result) {
+			System.out.println("Valid first name");
+		} else {
+			System.out.println("Pattern not matched");
+		}
+
+	}
+
+}
+
+
+	
