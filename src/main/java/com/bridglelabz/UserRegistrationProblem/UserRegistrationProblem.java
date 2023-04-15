ea01fc4eec70a5662c44fa61c11c15fd04755883
sahana199835 <tnsahana1@gmail.com>
2023-04-15 18:10:00 +0530

    add:check valid email

--- src/main/java/com/bridglelabz/UserRegistrationProblem/UserRegistrationProblem.java
+++ src/main/java/com/bridglelabz/UserRegistrationProblem/UserRegistrationProblem.java
@@ -9,28 +9,29 @@ public class UserRegistrationProblem {
 	public static void main(String[] args) {
 		String regex = "^[A-Z][a-zA-Z]{2,}$";
 
+		String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter The First Name : ");
-		String data = sc.nextLine();
 		System.out.println("Enter The Last Name : ");
 		String lastName = sc.nextLine();
+		System.out.println("Enter Email Address: ");
+		String data = sc.nextLine();
 
 		Pattern pattern = Pattern.compile(regex);
 
-		Matcher matcher = pattern.matcher(data);
 		Matcher matcher = pattern.matcher(lastName);
+		Matcher matcher = pattern.matcher(data);
 		boolean result = matcher.matches();
 
 		if (result) {
-			System.out.println("Valid first name");
 			System.out.println("Valid first name and last name");
+			System.out.println("Valid Email");
 		} else {
 			System.out.println("Pattern not matched");
+			System.out.println("Invalid Email");
 		}
 
 	}
-
 }
-
 
 	
