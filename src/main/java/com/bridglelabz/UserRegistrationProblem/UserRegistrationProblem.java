e79b0ee7cc260420f8cad956bb37a3666f407a8c
sahana199835 <tnsahana1@gmail.com>
2023-04-15 18:25:03 +0530

    add:check the password

--- src/main/java/com/bridglelabz/UserRegistrationProblem/UserRegistrationProblem.java
+++ src/main/java/com/bridglelabz/UserRegistrationProblem/UserRegistrationProblem.java
@@ -7,27 +7,28 @@ import java.util.regex.Pattern;
 public class UserRegistrationProblem {
 
 	public static void main(String[] args) {
-		String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-		String regex = "^[1-9][0-9]\\s[1-9][0-9]{9}";
+		 regex = "^[1-9][0-9]\\s[1-9][0-9]{9}";
+			String regex = "^.{8,}$";
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Email Address: ");
-		String data = sc.nextLine();
-		System.out.println("Enter Mobile Number: ");
-		String mobileNO = sc.nextLine();
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter Mobile Number: ");
+			String mobileNO = sc.nextLine();
+			System.out.println("Enter A Password: ");
+			String password = sc.nextLine();
 
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(data);
-		Matcher matcher = pattern.matcher(mobileNO);
-		boolean result = matcher.matches();
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(mobileNO);
+			Matcher matcher = pattern.matcher(password);
+			boolean result = matcher.matches();
 
-		if (result) {
-			System.out.println("Valid Email");
-			System.out.println("Valid Mobile Number");
-		} else {
-			System.out.println("Invalid Email");
-			System.out.println("Invalid Mobile Number");
+			if (result) {
+				System.out.println("Valid Mobile Number");
+				System.out.println("You have entered a Valid Password");
+			} else {
+				System.out.println("Invalid Mobile Number");
+				System.out.println("Invalid Password");
+			}
+
 		}
-
 	}
-	
+	}
