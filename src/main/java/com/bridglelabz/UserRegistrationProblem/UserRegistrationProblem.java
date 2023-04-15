3373e2e9e263793beab1ffd28b98eea903323a10
sahana199835 <tnsahana1@gmail.com>
2023-04-15 18:30:26 +0530

    add:check uppercase password

--- src/main/java/com/bridglelabz/UserRegistrationProblem/UserRegistrationProblem.java
+++ src/main/java/com/bridglelabz/UserRegistrationProblem/UserRegistrationProblem.java
@@ -7,28 +7,24 @@ import java.util.regex.Pattern;
 public class UserRegistrationProblem {
 
 	public static void main(String[] args) {
-		 regex = "^[1-9][0-9]\\s[1-9][0-9]{9}";
-			String regex = "^.{8,}$";
+		String regex = "^.{8,}$";
+		String regex = "^(?=.*[A-Z]).{8,}$";
 
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter Mobile Number: ");
-			String mobileNO = sc.nextLine();
-			System.out.println("Enter A Password: ");
-			String password = sc.nextLine();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter A Password: ");
+		System.out.println("Enter a password having atleast one uppercase : ");
+		String password = sc.nextLine();
 
-			Pattern pattern = Pattern.compile(regex);
-			Matcher matcher = pattern.matcher(mobileNO);
-			Matcher matcher = pattern.matcher(password);
-			boolean result = matcher.matches();
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(password);
+		boolean result = matcher.matches();
 
-			if (result) {
-				System.out.println("Valid Mobile Number");
-				System.out.println("You have entered a Valid Password");
-			} else {
-				System.out.println("Invalid Mobile Number");
-				System.out.println("Invalid Password");
-			}
-
+		if (result) {
+			System.out.println("You have entered a Valid Password");
+		} else {
+			System.out.println("Invalid Password");
 		}
+
 	}
-	}
+}
+}
